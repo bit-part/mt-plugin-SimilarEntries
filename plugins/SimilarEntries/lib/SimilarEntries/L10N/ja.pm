97094c00b7db50d815860381a52bacb5aeaa37fd
tinybeans <register@tinybeans.net>
2015-06-24 12:47:17 +0900

    プラグイン設定用の多言語情報を削除

--- plugins/SimilarEntries/lib/SimilarEntries/L10N/ja.pm
+++ plugins/SimilarEntries/lib/SimilarEntries/L10N/ja.pm
@@ -11,12 +11,6 @@ use vars qw( %Lexicon );
     'bit part LLC' => 'bit part 合同会社',
     'description of SimilarEntries' => 'SimilarEntriesの説明',
 
-    # config_template.tmpl
-    'JSON URL to relate' => '関連付け用JSONのURL',
-    'Enter the JSON URL to associate entries.' => '記事を関連性を割り出すための JSON の URL を入力します',
-    'JSON URL to output HTML' => '出力用JSONのURL',
-    'Enter the JSON URL to output HTML.' => '最終的に出力する内容が記載された JSON の URL を入力します',
-    'Enter JavaScript to output by the tag of MTSimilarEntriesShow.' => 'MTSimilarEntriesShow タグで出力する JavaScript を入力します',
 
     # ContextHandlers.pm
     'The fields modifier is required.' => 'fieldsモディファイアは必須です。'
